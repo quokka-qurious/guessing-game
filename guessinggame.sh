b5c4ea8d4962ade1f157754617b7314b7fa8d320
quokka-qurious <jessica.je.mcbroom@gmail.com>
2023-11-13 13:21:47 +1100

    multiple guesses now allowed

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,16 +5,14 @@ read guess
 # count how many files are in the directory
 correct_answer=$(ls | wc -l)
 
-# check if the guess was correct
-if [[ $guess -eq $correct_answer ]]
-then
-  echo "Correct!"
-else
-  echo "Incorrect!"
-fi
+# ask for a guess until the user gets it right
+while [[ $guess -ne $correct_answer ]]
+do
+  echo "Wrong - there are not $guess files in the current directory."
+  echo "Try again."
+  read guess
+done
 
-# output the guess and answer
-echo "Your Guess:"
-echo "$guess"
-echo "The answer:"
-echo "$correct_answer"
+# congratulate the user
+echo "Well done! $guess is the correct answer."
+
