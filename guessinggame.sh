f30ff206160cfb50fdf197e43a16cf80195a309f
quokka-qurious <jessica.je.mcbroom@gmail.com>
2023-11-13 13:15:25 +1100

    added correctness check to guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,6 +5,14 @@ read guess
 # count how many files are in the directory
 correct_answer=$(ls | wc -l)
 
+# check if the guess was correct
+if [[ $guess -eq $correct_answer ]]
+then
+  echo "Correct!"
+else
+  echo "Incorrect!"
+fi
+
 # output the guess and answer
 echo "Your Guess:"
 echo "$guess"
