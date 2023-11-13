49c3e6530654341d58cb94de89e951222c4bc2e4
quokka-qurious <jessica.je.mcbroom@gmail.com>
2023-11-13 13:10:52 +1100

    updated guessinggame.sh to also calculate the correct answer

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,12 @@
+# prompt the user for a guess
 echo "How many files are in the current directory?"
 read guess
+
+# count how many files are in the directory
+correct_answer=$(ls | wc -l)
+
+# output the guess and answer
+echo "Your Guess:"
 echo "$guess"
+echo "The answer:"
+echo "$correct_answer"
